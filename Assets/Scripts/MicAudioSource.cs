d2e1767d2f8c7016ae4a3e9d566cad18248d639e
Kotaro Sonoda <kotaro@nagasaki-u.ac.jp>
2024-07-12 16:35:00 +0900

    rename 'Script'-folder to 'Scripts' and AudioScene added with the MicAudioSource.cs

---
+++ Assets/Scripts/MicAudioSource.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicAudioSource : MonoBehaviour
+{
+    // Variables
+    // - Serialize
+    [SerializeField] private string m_MicInDeviceName;
+    [SerializeField, Range(10, 300)] private float m_AmpGain = 100;
+    [SerializeField] private GameObject m_Cube;
+    // - Const
+    private const int SAMPLE_RATE = 44100;
+    private const float BUFFER_TIME = 0.05f;
+    private int BUFFER_SIZE = Mathf.CeilToInt(SAMPLE_RATE * BUFFER_TIME);
+    // - AudioSource
+    private AudioSource m_MicAudioSource;
+    private float[] waveData;
+
+    // Awake
+    private void Awake()
+    {
+        m_MicAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string targetDevice = "";
+        foreach (var device in Microphone.devices) {
+            Debug.Log($"Device Name: {device}");
+            if (device.Equals(m_MicInDeviceName)) {
+                targetDevice = device;
+            }
+        }
+
+        Debug.Log($"=== Device Set: {targetDevice} ===");
+        MicStart(targetDevice);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_MicAudioSource.isPlaying)
+            return;
+
+        waveData = new float[BUFFER_SIZE];
+        m_MicAudioSource.GetOutputData(waveData, 0);
+
+        float audioLevel = waveData.Average(Mathf.Abs);
+        m_Cube.transform.localScale = new Vector3(1, 1 + m_AmpGain * audioLevel, 1);
+    }
+
+    private void MicStart(string device) {
+        if (device.Equals("")) return;
+
+        m_MicAudioSource.clip = Microphone.Start(device, true, 1, SAMPLE_RATE);
+
+        //マイクデバイスの準備ができるまで待つ
+        while (!(Microphone.GetPosition("") > 0)) { }
+
+        m_MicAudioSource.Play();
+    }
+}
