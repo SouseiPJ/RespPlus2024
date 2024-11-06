70a0e5cdfd1b4d50357965ad272787ed7533af3e
HONDANOPC\kana <kqnnamirucha@icloud.com>
2024-11-07 03:26:44 +0900

    レポート終わった。ポケモンカードしたい泣

---
+++ Assets/Script/メインシーン/LabSceneTo.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LabSceneTo : MonoBehaviour
+{
+    // ボタンが押された時に呼び出される関数
+    public void OnButtonPress()
+    {
+        // 次のシーンに切り替える。シーン名を次のシーン名に置き換えてください。
+        SceneManager.LoadScene("LabScene");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
